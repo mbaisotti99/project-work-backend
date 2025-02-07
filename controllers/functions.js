80a8a981668d982172ae1d90ff21cc6ef65320c1
davidetoscano129 <toscano.davidew@gmail.com>
2025-02-07 11:31:36 +0100

    add: routes

--- controllers/functions.js
+++ controllers/functions.js
@@ -2,15 +2,31 @@ const connection = require("../data/doc_db")
 
 // INDEX
 const indexMed = (req, resp, next) => {
-    const sql = `   SELECT * 
-                    FROM medici    `
 
-    connection.query(sql, (err, docs) => {
+    const filters = req.query;
+
+    let sql = ` SELECT * FROM medici `
+
+    const params = [];
+    const conditions = [];
+
+    if (filters.search) {
+        conditions.push("nome LIKE ? OR cognome LIKE ?");
+        params.push(`%${filters.search}%`, `%${filters.search}%`);
+    }
+
+    if (filters.specializzazione) {
+        conditions.push("specializzazione = ?");
+        params.push(filters.specializzazione);
+    }
+
+    if (conditions.length > 0) {
+        sql += ` WHERE ${conditions.join(" AND ")}`;
+    }
+
+    connection.query(sql, params, (err, docs) => {
         if (err) {
             return next(new Error("Errore del server"))
-            // return resp.status(500).json({
-            //     err: err.stack
-            // })
         } else {
             resp.status(200).json({
                 message: "Medici trovati",
@@ -26,7 +42,7 @@ const showMed = (req, resp, next) => {
 
 
     const id = parseInt(req.params.id)
-    if (!isNaN(id)){
+    if (!isNaN(id)) {
         connection.query(sql, (err) => {
             if (err) {
                 return next(new Error("Internal Server Error"));
@@ -38,15 +54,19 @@ const showMed = (req, resp, next) => {
                 connection.query(sql2, [id], (err, results) => {
                     if (err) {
                         return next(new Error("Internal Server Error"))
-                    } else {
+                    } else if (results.length !== 0) {
                         return resp.status(200).json({
-                            message: "Medico Trovato", // Da rifare. nota: malfunzionante su postman
+                            message: "Medico Trovato",
                             data: results
                         })
+                    } else {
+                        return resp.status(404).json({
+                            message: "Medico NON Trovato"
+                        })
                     }
-    
-    
-    
+
+
+
                 })
             }
         })
@@ -104,4 +124,37 @@ const showRev = (err, req, resp, next) => {
     })
 }
 
-module.exports = { indexMed, showMed, showRev }
+
+const createMed = (req, resp, next) => {
+    const { nome, cognome, mail, cell, indirizzo, spec } = req.body
+    const mailDupes = []
+    const sql = `
+        INSERT INTO medici (nome, cognome, email, telefono, indirizzo, specializzazione)
+        VALUES (?, ?, ?, ?, ?, ?)
+    `
+
+    const controlSql = `SELECT email FROM medici WHERE email = ?`
+    connection.query(controlSql, [mail], (err, result) => {
+        if (err) {
+            return next(new Error("Errore nel controllo"))
+        } else if (result.length !== 0) {
+            return resp.send(500).json({
+                message: "Mail già presente"
+            })
+        } else {
+            connection.query(sql, [nome, cognome, mail, cell, indirizzo, spec], (err, results) => {
+                if (err) {
+                    return next(new Error("Medico non caricato"))
+                } else {
+                    return resp.status(500).json({
+                        message: "OK! Medico caricato"
+                    })
+                }
+            })
+        }
+    })
+
+}
+
+
+module.exports = { indexMed, showMemodule, createMed, showRev }
--- routes/routes.js
+++ routes/routes.js
@@ -1,10 +1,11 @@
 const express = require("express")
 
-const { indexMed, showRev, showMed } = require("../controllers/functions")
+const { indexMed, showRev, showMed, createMed } = require("../controllers/functions")
 
 const router = express.Router()
 
 router.get("/", indexMed)
+router.post("/post", createMed)
 router.get("/:id", showMed)
 router.get("/:id/recensioni", showRev)
 
