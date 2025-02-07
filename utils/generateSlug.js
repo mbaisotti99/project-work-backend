32efe82f0bc3578cc74b1e167625d1ffc82c4f03
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2025-02-07 15:53:46 +0100

    refactoring: store route has validation now

---
+++ utils/generateSlug.js
@@ -0,0 +1,11 @@
+const generateSlug = (nome, cognome) => {
+    return `${nome} ${cognome}` // Combina nome e cognome
+        .toLowerCase() // Converte in minuscolo
+        .normalize("NFD").replace(/[\u0300-\u036f]/g, "") // Rimuove accenti
+        .replace(/[^a-z0-9\s]/g, "") // Rimuove caratteri speciali
+        .replace(/\s+/g, "-"); // Sostituisce spazi con trattini
+};
+
+module.exports = {
+    generateSlug,
+}
