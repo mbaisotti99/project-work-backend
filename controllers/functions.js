0d859a92395a94924cebc0903511101928c5456d
mbaisotti99 <matteo.baisotti@gmail.com>
2025-02-07 10:11:40 +0100

    controlli base su rotte

--- controllers/functions.js
+++ controllers/functions.js
@@ -25,27 +25,33 @@ const showMed = (req, resp, next) => {
     const sql = ` SELECT * FROM medici; `;
 
 
-    const id = req.params.id
-    connection.query(sql, (err) => {
-        if (err) {
-            return next(new Error("Internal Server Error"));
-        } else {
-            const sql2 = `  SELECT *
-                            FROM medici
-                            WHERE id = ?                
-            `
-            connection.query(sql2, [id], (err, results) => {
-                if (err) throw new Error("Internal Server Error")
-
-
-                return resp.status(200).json({
-                    message: "Medico non Trovato", // Da rifare. nota: malfunzionante su postman
-                    data: results
+    const id = parseInt(req.params.id)
+    if (!isNaN(id)){
+        connection.query(sql, (err) => {
+            if (err) {
+                return next(new Error("Internal Server Error"));
+            } else {
+                const sql2 = `  SELECT *
+                                FROM medici
+                                WHERE id = ?                
+                `
+                connection.query(sql2, [id], (err, results) => {
+                    if (err) throw new Error("Internal Server Error")
+    
+    
+                    return resp.status(200).json({
+                        message: "Medico non Trovato", // Da rifare. nota: malfunzionante su postman
+                        data: results
+                    })
+    
                 })
-
-            })
-        }
-    })
+            }
+        })
+    } else {
+        return resp.status(500).json({
+            message: "Id non valido"
+        })
+    }
 }
 
 // SHOW RECENSIONI
@@ -65,7 +71,7 @@ const showRev = (err, req, resp, next) => {
             return next(new Error("Errore del server"))
         } else {
 
-            const id = req.params.id
+            const id = parseInt(req.params.id)
 
             const sql2 = `
                 SELECT medici.nome as nome_medico, medici.cognome as cognome_medico, utenti.nome_utente as utente, recensioni.recensione, recensioni.voto
@@ -75,17 +81,22 @@ const showRev = (err, req, resp, next) => {
                 JOIN utenti
                 ON utenti.id = recensioni.id_utente
             `
-
-            connection.query(sql2, [id], (err, reviews) => {
-                if (err) {
-                    return next(new Error("Errore del server"))
-                } else {
-                    resp.status(200).json({
-                        message: "Recensioni trovate",
-                        data: reviews
-                    })
-                }
-            })
+            if (!isNaN(id)) {
+                connection.query(sql2, [id], (err, reviews) => {
+                    if (err) {
+                        return next(new Error("Errore del server"))
+                    } else {
+                        resp.status(200).json({
+                            message: "Recensioni trovate",
+                            data: reviews
+                        })
+                    }
+                })
+            } else {
+                return resp.status(500).json({
+                    message: "Id non valido"
+                })
+            }
         }
     })
 }
